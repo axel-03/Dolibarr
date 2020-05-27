88430631de9de7e55bcd7b95ce0ecba2c39e4d8c
ulises10-code <65869982+ulises10-code@users.noreply.github.com>
2020-05-26 23:09:54 -0500

    Update generate_filelist_xml.php

--- build/generate_filelist_xml.php
+++ build/generate_filelist_xml.php
@@ -1,26 +1,5 @@
 #!/usr/bin/env php
 <?php
-/* Copyright (C) 2015-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
-/**
- *      \file       build/generate_filelist_xml.php
- *		\ingroup    dev
- * 		\brief      This script create a xml checksum file
- */
 
 if (! defined('NOREQUIREDB')) define('NOREQUIREDB', '1');	// Do not create database handler $db
 
